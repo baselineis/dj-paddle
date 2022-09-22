abce23d8cea505ff9c115d4b4623a0d9d3dc0219
Gilli Sigurdsson <gilli@gilli.is>
2022-09-22 20:33:28 +0300

    Update admin.py

--- djpaddle/admin.py
+++ djpaddle/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 
 from . import models
+from .models import Checkout, Plan
 
-admin.site.register(models.Checkout)
+admin.site.register(Checkout)
 
 
 class PriceInline(admin.TabularInline):
     model = models.Price
 
 
-@admin.register(models.Plan)
+@admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
     inlines = (PriceInline,)
     list_display = (
